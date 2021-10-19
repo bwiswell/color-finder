6a659b3d7c822be5e588a0987df4db21d7d5c6b7
Ben Wiswell <benjwiswell@gmail.com>
2021-10-19 10:54:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+"use strict";
+exports.__esModule = true;
+var canvas_1 = require("canvas");
+var node_vibrant_1 = require("node-vibrant");
+var ColorFinder = /** @class */ (function () {
+    function ColorFinder(src, canvas, maxColors) {
+        this.maxColors = 5;
+        this.src = src;
+        this.canvas = canvas;
+        this.maxColors = maxColors;
+        this.pixels = [];
+        var context = this.canvas.getContext('2d');
+        var x, y, imageData, hex;
+        for (x = 0; x < this.canvas.width; x++) {
+            var column = [];
+            for (y = 0; y < this.canvas.height; y++) {
+                imageData = context.getImageData(x, y, 1, 1);
+                hex = this._toHex(imageData[0], imageData[1], imageData[2]);
+                column.push(hex);
+            }
+            this.pixels.push(column);
+        }
+    }
+    ColorFinder.prototype._hexDist = function (a, b) {
+        return this._rgbDist(this._toRGB(a), this._toRGB(b));
+    };
+    ColorFinder.prototype._nearestPixel = function (color) {
+        var minDist = 10000;
+        var nearestPixel = { x: 0, y: 0 };
+        var x, y, dist;
+        for (x = 0; x < this.canvas.width; x++) {
+            for (y = 0; y < this.canvas.height; y++) {
+                dist = this._hexDist(color, this.pixels[x][y]);
+                if (dist < minDist) {
+                    minDist = dist;
+                    nearestPixel = { x: x, y: y };
+                }
+            }
+        }
+        return nearestPixel;
+    };
+    ColorFinder.prototype._toScaledPos = function (px, py, layout) {
+        var xScale = this.canvas.width === 0 ? 0 : layout.width / this.canvas.width;
+        var yScale = this.canvas.height === 0 ? 0 : layout.height / this.canvas.height;
+        var dx = px * xScale;
+        var dy = py * yScale;
+        return { x: dx, y: dy };
+    };
+    ColorFinder.prototype._toHex = function (r, g, b) {
+        return ((r << 16) | (g << 8) | b).toString(16).slice(-6);
+    };
+    ColorFinder.prototype._toImgPos = function (dx, dy, layout) {
+        var xScale = this.canvas.width === 0 ? 0 : layout.width / this.canvas.width;
+        var yScale = this.canvas.height === 0 ? 0 : layout.height / this.canvas.height;
+        var px = xScale === 0 ? 0 : (dx - layout.left) / xScale;
+        var py = yScale === 0 ? 0 : (dy - layout.top) / yScale;
+        return { x: px, y: py };
+    };
+    ColorFinder.prototype._toRGB = function (hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    };
+    ColorFinder.prototype._rgbDist = function (a, b) {
+        var rDiff = (a.r - b.r);
+        var gDiff = (a.g - b.g);
+        var bDiff = (a.b - b.b);
+        return Math.sqrt(Math.pow(rDiff, 2) + Math.pow(gDiff, 2) + Math.pow(bDiff, 2));
+    };
+    ColorFinder.prototype.colorAtPos = function (x, y, layout) {
+        var _a = this._toImgPos(x, y, layout), px = _a.x, py = _a.y;
+        return this.pixels[x][y];
+    };
+    ColorFinder.prototype.locateColor = function (color, layout) {
+        var _a = this._nearestPixel(color), px = _a.x, py = _a.y;
+        return this._toScaledPos(px, py, layout);
+    };
+    ColorFinder.prototype.mainColors = function () {
+        return node_vibrant_1["default"].from(this.src)
+            .maxColorCount(this.maxColors)
+            .getSwatches()
+            .then(function (swatches) {
+            var colors = [];
+            for (var swatch in swatches) {
+                colors.push(swatches[swatch].hex);
+            }
+            return colors;
+        });
+    };
+    return ColorFinder;
+}());
+var createColorFinder = function (src, maxColors) {
+    if (maxColors === void 0) { maxColors = 5; }
+    return canvas_1.loadImage(src)
+        .then(function (image) {
+        var canvas = canvas_1.createCanvas(image.width, image.height);
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+        return new ColorFinder(src, canvas, maxColors);
+    });
+};
+exports.ColorFinder = ColorFinder;
+exports.createColorFinder = createColorFinder;
---
+++ index.ts
@@ -0,0 +1,133 @@
+import { createCanvas, loadImage } from 'canvas';
+import Vibrant from 'node-vibrant';
+
+import type { Canvas } from 'canvas';
+
+type Layout = {
+    left: number,
+    top: number,
+    width: number,
+    height: number
+};
+type Pos = { x: number, y: number };
+type RGB = { r: number, g: number, b: number };
+
+class ColorFinder {
+
+    src: string;
+    canvas: Canvas;
+    maxColors: number = 5;
+    pixels: string[][];
+
+    constructor (src: string, canvas: Canvas, maxColors: number) {
+        this.src = src;
+        this.canvas = canvas;
+        this.maxColors = maxColors;
+
+        this.pixels = [];
+        const context = this.canvas.getContext('2d');
+        let x, y, imageData, hex;
+        for (x = 0; x < this.canvas.width; x++) {
+            const column: string[] = []
+            for (y = 0; y < this.canvas.height; y++) {
+                imageData = context.getImageData(x, y, 1, 1);
+                hex = this._toHex(imageData[0], imageData[1], imageData[2]);
+                column.push(hex);
+            }
+            this.pixels.push(column);
+        }
+    }
+
+    _hexDist (a: string, b: string): number {
+        return this._rgbDist(this._toRGB(a), this._toRGB(b));
+    }
+
+    _nearestPixel (color: string): Pos {
+        let minDist = 10000;
+        let nearestPixel = { x: 0, y: 0 };
+        let x, y, dist;
+        for (x = 0; x < this.canvas.width; x++) {
+            for (y = 0; y < this.canvas.height; y++) {
+                dist = this._hexDist(color, this.pixels[x][y]);
+                if (dist < minDist) {
+                    minDist = dist;
+                    nearestPixel = { x: x, y: y };
+                }
+            }
+        }
+        return nearestPixel;
+    }
+
+    _toScaledPos (px: number, py: number, layout: Layout): Pos {
+        const xScale = this.canvas.width === 0 ? 0 : layout.width / this.canvas.width
+        const yScale = this.canvas.height === 0 ? 0 : layout.height / this.canvas.height
+        const dx = px * xScale
+        const dy = py * yScale
+        return { x: dx, y: dy }
+    }
+
+    _toHex (r: number, g: number, b: number): string {
+        return ((r << 16) | (g << 8) | b).toString(16).slice(-6);
+    }
+
+    _toImgPos (dx: number, dy: number, layout: Layout): Pos {
+        const xScale = this.canvas.width === 0 ? 0 : layout.width / this.canvas.width
+        const yScale = this.canvas.height === 0 ? 0 : layout.height / this.canvas.height
+        const px = xScale === 0 ? 0 : (dx - layout.left) / xScale
+        const py = yScale === 0 ? 0 : (dy - layout.top) / yScale
+        return { x: px, y: py }
+    }
+
+    _toRGB (hex: string): RGB {
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : null;
+    }
+
+    _rgbDist (a: RGB, b: RGB): number {
+        const rDiff = (a.r - b.r);
+        const gDiff = (a.g - b.g);
+        const bDiff = (a.b - b.b);
+        return Math.sqrt(rDiff ** 2 + gDiff ** 2 + bDiff ** 2);
+    }
+
+    colorAtPos (x: number, y: number, layout: Layout): string {
+        const { x: px, y: py } = this._toImgPos(x, y, layout);
+        return this.pixels[x][y];
+    }
+
+    locateColor (color: string, layout: Layout): Pos {
+        const { x: px, y: py } = this._nearestPixel(color);
+        return this._toScaledPos(px, py, layout);
+    }
+
+    mainColors (): Promise<string[]> {
+        return Vibrant.from(this.src)
+        .maxColorCount(this.maxColors)
+        .getSwatches()
+        .then(swatches => {
+            const colors: string[] = [];
+            for (let swatch in swatches) {
+                colors.push(swatches[swatch].hex);
+            }
+            return colors;
+        })
+    }
+}
+
+const createColorFinder = (src: string, maxColors: number = 5): Promise<ColorFinder> => {
+    return loadImage(src)
+    .then(image => {
+        const canvas = createCanvas(image.width, image.height);
+        const context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+        return new ColorFinder(src, canvas, maxColors);
+    })
+}
+
+
+exports.ColorFinder = ColorFinder;
+exports.createColorFinder = createColorFinder;
